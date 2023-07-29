d9f71476f66958404a260d4541766f739d715619
Cristiano Revil <49713716+cristianorevil@users.noreply.github.com>
2023-07-29 10:03:15 +0200

    first commti

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# cc_bulk_import
-A plugin to bulk import urls and documents in cheshire cat rabbit hole
+# What is this?
+
+This is a plugin (tool) for the [Cheshire Cat Project](https://github.com/pieroit/cheshire-cat), which allows you to bulk import URLS and Documents into the Rabbit Hole.
+
+# Install
+
+Download the **cc_bulk_import** folder into the **cheshire-cat/core/cat/plugins** one.
+Create a folder "bulkimport" into the **/core/cat/static** one.
+
+# Usage
+
+**IMPORT URLS**
+Ask the cat """bulkimport url **LIST OF URLS COMMA SEPARATED**"
+Example: bulkimport url https://cheshirecat.ai/code-of-ethics/,https://cheshirecat.ai/
+
+**IMPORT DOCUMENTS**
+Put a **COPY** (after import files wil be deleted, so put a copy not the original one) of files you want to import into **/core/cat/static/bulkimport** folder and then ask the cat """bulkimport docs"
+
+# Output
+
+The cat will give a feedback after the import process is completed, so if you have multiple urls on multiple documenta you need to wait for having a response from the cat.
+
+The cat responde with a table containing the status (OK or KO) for any single url/document setn to the rabbit hole.
---
+++ bulik_import.py
@@ -0,0 +1,48 @@
+import requests
+import os
+
+from cat.mad_hatter.decorators import tool, hook
+from cat.log import log
+
+
+@tool(return_direct=True)
+def bulk_url_import(url_list: str, cat):
+    """Replies to "bulkimport url". Input is the comma separated list of urls."""
+    message = "<table><thead><tr><th class=\"text-neutral\">URL</th><th class=\"text-neutral\">Status</th></tr></thead><tbody>"
+    for link in url_list.split(","):
+        if not link.startswith(('http://', 'https://')):
+            message = message + link + ": Invalid link format: must start with 'http://' or 'https://'\n"
+        else:
+            try:
+                parsed_link = requests.get(link).url
+                log("Send " + parsed_link + " to rabbithole", "WARNING")
+                cat.rabbit_hole.ingest_file(parsed_link, 400, 100)
+                log(parsed_link + " sent to rabbithole!", "WARNING")
+                message = message + "<tr><td>" + link + "</td><td>&#x2705;</td></tr>"
+            except requests.exceptions.RequestException as err:
+                message = message + "<tr><td>" + link + \
+                    "</td><td>&#x274C; " + str(err) + "</td></tr>"
+    message = message + "</tbody></table>"
+    return message
+
+@tool(return_direct=True)
+def bulk_docs_import(tool_input,cat):
+    """Replies to "bulkimport docs". Input is always none"""
+
+    message = "<table><thead><tr><th class=\"text-neutral\">Document</th><th class=\"text-neutral\">Status</th></tr></thead><tbody>"
+    files = os.listdir("/app/cat/static/bulkimport")
+    for file in files:
+       if file.startswith('.'):
+           continue
+       try:
+            log("Send " + file + " to rabbithole", "WARNING")
+            filepath = "/app/cat/static/bulkimport/" + file
+            cat.rabbit_hole.ingest_file(filepath, 400, 100)
+            log(file + " sent to rabbithole!", "WARNING")
+            message = message + "<tr><td>" + file + "</td><td>&#x2705;</td></tr>"
+            os.remove(filepath)
+       except requests.exceptions.RequestException as err:
+            message = message + "<tr><td>" + file + \
+                "</td><td>&#x274C; " + str(err) + "</td></tr>"
+    message = message + "</tbody></table>"
+    return message
